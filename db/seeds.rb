8c384ffe204b6933ba1f063a7259d3fefa4ec4e4
Rachael Lunney <email": null>
2016-10-26 18:28:17 -0500

    Add seed data and html files for article seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'slim'
+
 user_seeds = [
   { username: "rach", email: "rach@sandbox.com", password: "password" },
   { username: "will", email: "will@sandbox.com", password: "password" },
@@ -11,5 +13,9 @@ user_seeds.each { |user| User.create(user) }
 10.times { Article.create }
 
 version_seeds = [
-  { title: "Turtles", body: "Turtles are great!", article_id: , editor_id:  },
+  { title: "Turtle", body: File.open("#{Rails.root}/db/seed_files/turtle.html").read , article_id: 1, editor_id: 1 },
+  { title: "Bat", body: File.open("#{Rails.root}/db/seed_files/bat.html").read , article_id: 2, editor_id: 2 },
+  { title: "Caterpiller", body: File.open("#{Rails.root}/db/seed_files/caterpiller.html").read , article_id: 3, editor_id: 3 }
 ]
+
+version_seeds.each { |version| Version.create(version) }
