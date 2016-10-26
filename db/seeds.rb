baafd4b01e035b5ab3ac8a49b030490f25454652
Rachael Lunney <email": null>
2016-10-26 16:35:51 -0500

    Add seed data for user and articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user_seeds = [
+  { username: "rach", email: "rach@sandbox.com", password: "password" },
+  { username: "will", email: "will@sandbox.com", password: "password" },
+  { username: "sayam", email: "sayam@sandbox.com", password: "password" },
+  { username: "john", email: "john@sandbox.com", password: "password" },
+  { username: "nick", email: "nick@sandbox.com", password: "password" },
+]
+
+user_seeds.each { |user| User.create(user) }
+
+10.times { Article.create }
+
+version_seeds = [
+
+]
