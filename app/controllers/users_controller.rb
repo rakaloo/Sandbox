b629ba092f0ac51836c3be49af9502964f9d1c89
Will Farrell <email": null>
2016-10-27 14:13:01 -0500

    Add edit function straight to user_admin index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,20 +8,21 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-    @user = User.find_by(id: params[:id])
-    p "Got through here!"
-    p params
-  end
-
   def update
-    p "Got through the update path!"
-    p params
-
+    @user = User.find_by(id: params[:id])
+    if @user.update_attributes(update_user_role_params)
+      redirect_to users_admin_index_path
+    else
+      redirect_to users_admin_index_path
+    end
   end
 
   def destroy
   end
 
+  private
 
+  def update_user_role_params
+      params.require(:user).permit(:role)
+  end
 end
