895ec15643bed367335bc8dceed608b2d8fb136f
Rachael Lunney <email": null>
2016-10-27 16:59:55 -0500

    Add categorization creation to article creation form

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,6 +5,7 @@ class Article < ApplicationRecord
 	has_many :editors, through: :versions, source: :editor
 
 	accepts_nested_attributes_for :versions
+  accepts_nested_attributes_for :categorizations
 
 	def current_version
 		versions.last
