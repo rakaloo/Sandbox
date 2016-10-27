01efe0e740fbbc7068f6646eb7f4a094128e6e3d
Will Farrell <email": null>
2016-10-27 17:53:29 -0500

    Add validations for categorization

--- spec/models/categorization_spec.rb
+++ spec/models/categorization_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Categorization, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    let (:article) { Article.create}
+    let (:category) { Category.create(name: "Clouds") }
+    # let (:categorization_1) { Categorization.create(article)}
+    # let (:categorization_2) { Categorization.create(article: article, category: category) }
+
+    it "is not valid without an article" do
+      categorization = Categorization.new(category: category)
+      expect(categorization.valid?).to eq false
+    end
+
+    it "is not valid without an article" do
+      categorization = Categorization.new(article: article)
+      expect(categorization.valid?).to eq false
+    end
+
+    it "is valid with an article and a category" do
+      categorization = Categorization.create(article: article, category: category)
+      expect(categorization.valid?).to eq true
+    end
+  end
+
+  # describe "associations" do
+  #   let!(:category) { Category.create(name: "potato") }
+  #   let!(:another_category) { Category.create(name: "onion") }
+
+  #   let (:articles) { [Article.create(), Article.create()] }
+
+  #   let (:categorizations) { [Categorization.create(article: articles[0], category: category), Categorization.create(article: articles[1], category: category), Categorization.create(article: articles[1], category: another_category), Categorization.create(article: articles[0], category: another_category)] }
+
+  #   it "has many articles" do
+  #     expect(category.articles).to all be_a Article
+  #   end
+
+  #   it "has many categorizations" do
+  #     expect(category.categorizations).to all be_a Categorization
+  #   end
+
+  # end
 end
