b24dbfabb2ee0ba26f7d65955efbb9b36f416add
Will Farrell <email": null>
2016-10-27 18:51:57 -0500

    Add show tests to versions controller.  Pass 7/7 tests

--- spec/controllers/versions_controller_spec.rb
+++ spec/controllers/versions_controller_spec.rb
@@ -28,5 +28,26 @@ RSpec.describe VersionsController, type: :controller do
     end
   end
 
+  describe "GET #show" do
+    let!(:user) { User.create(username: "duke", email:"duke@duke.com", password: "password") }
+    let!(:article) { Article.create! }
+    let (:versions) { [Version.create(title: "it's the first test title", body: "it's the first test body", article: article, editor: user ), Version.create(title: "it's second test title", body: "it's  second test body", article: article, editor: user)] }
+
+    it "responds with status code 200" do
+      get :show, { params: { article_id: article.id, id: versions[0].id } }
+      expect(response).to have_http_status 200
+    end
+
+    it "passes through the version found to be shown as @version" do
+      get :show, { params: { article_id: article.id, id: versions[0].id } }
+      expect(assigns(:version)).to be_a Version
+    end
+
+    it "renders the :show template" do
+      get :show, { params: { article_id: article.id, id: versions[0].id } }
+      expect(response).to render_template(:show)
+    end
+  end
+
 
 end
