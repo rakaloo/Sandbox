992db023d0361b3d813adb3f07d584cf7fb0606b
Will Farrell <email": null>
2016-10-27 16:00:32 -0500

    Add tests for validations and attributes

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20161026200641) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string   "name"
+    t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
+
+  describe "reader and writer attributes" do
+    let (:category) { Category.create(name: "dogs") }
+
+    it "has a readable name" do
+      expect(category.name).to eq "dogs"
+    end
+
+    it "has a writable name" do
+      category.name = "popcorn"
+      expect(category.name).to eq "popcorn"
+    end
+
+  end
+
+  describe "validations" do
+    let!(:category) { Category.create(name: "potato") }
+
+    it "is not valid without an name" do
+      category = Category.new
+      expect(category.valid?).to eq false
+    end
+
+    it "is not valid if the name is not unique" do
+      category = Category.new(name: "potato")
+      expect(category.valid?).to eq false
+    end
+
+    it "is valid with a name" do
+      category = Category.new(name: "onion")
+      expect(category.valid?).to eq true
+    end
+  end
+
+  # describe "associations" do
+  #   let (:user) { User.create(username: "pentella", email: "npentella@gmail.com", password: "password") }
+
+  #   let (:articles) { [Article.create(), Article.create()] }
+
+  #   let (:versions) { [Version.create(title: "it's a test title", body: "it's a test body", article: articles[0], editor: user ), Version.create(title: "it's second test title", body: "it's  second test body", article: articles[1], editor: user)] }
+
+  #   it "has many versions" do
+  #     user.versions = versions
+  #     expect(user.versions[0]).to be_a Version
+  #   end
+
+  #   it "has many articles" do
+  #     user.articles = articles
+  #     expect(user.articles[0]).to be_a Article
+  #   end
+
+  end
   pending "add some examples to (or delete) #{__FILE__}"
 end
