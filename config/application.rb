a1e7dd709463d1238c9d7e7446aff464109c871c
Jonathan Warshaw <email": null>
2016-10-28 11:44:51 -0500

    Relocate ckeditor precompile commands

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,3 @@ module Sandbox
     # -- all .rb files in that directory are automatically loaded.
   end
 end
-
-config.assets.precompile += Ckeditor.assets
-config.assets.precompile += %w( ckeditor/* )
-config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
