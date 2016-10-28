02950c8d28dc13f04fcea795ea4cbab7808c6683
Will Farrell <email": null>
2016-10-28 15:48:37 -0500

    Error handling for version creation

--- app/models/version.rb
+++ app/models/version.rb
@@ -1,4 +1,7 @@
 class Version < ApplicationRecord
   belongs_to :article, required: false
   belongs_to :editor, class_name: "User"
+
+  validates :body, length: { minimum: 10 }
+  validates :title, length: { minimum: 2 }
 end
