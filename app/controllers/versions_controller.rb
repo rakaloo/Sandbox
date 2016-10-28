477c32e45c747855dd7f6f141924cf6368eef163
Rachael Lunney <email": null>
2016-10-27 19:06:40 -0500

    Add logic to create new version. Not currently functional

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -3,7 +3,7 @@ class VersionsController < ApplicationController
 		@article = Article.find_by(id: params[:article_id])
 		@versions = @article.versions
 	end
-	
+
 	def show
 		@version = Version.find_by(id: params[:id])
 	end
@@ -14,9 +14,9 @@ class VersionsController < ApplicationController
 
 	def create
 		@article = Article.find_by(id: params[:article_id])
-		@version = Version.new(version_params)
+		@version = Version.create(version_params.merge(editor: current_user))
+		p @version.errors.full_messages
 		@version.article = @article
-		@version.editor = current_user
 		redirect_to article_path(@article)
 	end
 
